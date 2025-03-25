71944818c7030b6a8788113d78eb229c16004a64
hedinang <tridunga1@gmail.com>
2025-03-26 02:26:01 +0700

    a

--- src/pages/cart/OrderList.js
+++ src/pages/cart/OrderList.js
@@ -1,4 +1,14 @@
-import { Button, Divider, Image, Modal, Pagination, Table } from "antd";
+import {
+  Button,
+  DatePicker,
+  Divider,
+  Image,
+  Input,
+  Modal,
+  Pagination,
+  Select,
+  Table,
+} from "antd";
 import { useEffect, useMemo, useState } from "react";
 import { FiTrash } from "react-icons/fi";
 import { IoMdRefresh } from "react-icons/io";
@@ -465,6 +475,258 @@ const OrderList = () => {
     <div className="order-list">
       <div className="font-semibold text-[20px] pl-[16px] pt-[16px]  flex items-center">
         Giỏ hàng
+      </div>
+      <div className="text-[20px] pl-[16px] pt-[16px] flex gap-[10px]">
+        <Input placeholder="Tìm kiếm mã user" className="w-[200px]" />
+        <Input placeholder="Tìm kiếm mã sản phẩm" className="w-[200px]" />
+        <Input placeholder="Tìm kiếm tên sản phẩm" className="w-[200px]" />
+        <Select
+          placeholder="Chọn loại sản phẩm"
+          allowClear
+          className="w-[200px]"
+          // onChange={onChooseCategory}
+          options={[
+            { value: "WATCH", label: "WATCH" },
+            { value: "BAG", label: "BAG" },
+            { value: "Jewelry", label: "Jewelry" },
+            { value: "Accessory", label: "Accessory" },
+            { value: "Fashion accessories", label: "Fashion accessories" },
+            { value: "Pottery", label: "Pottery" },
+            { value: "Camera", label: "Camera" },
+            { value: "Apparel", label: "Apparel" },
+            { value: "Shoes", label: "Shoes" },
+            { value: "Art quality", label: "Art quality" },
+            { value: "furniture", label: "furniture" },
+            { value: "bicycle", label: "bicycle" },
+            { value: "Consumer electronics", label: "Consumer electronics" },
+            { value: "Hobby", label: "Hobby" },
+            { value: "Sport", label: "Sport" },
+            { value: "Musical instrument", label: "Musical instrument" },
+            { value: "the expendables", label: "the expendables" },
+            { value: "game", label: "game" },
+            { value: "PC", label: "PC" },
+            { value: "Camping Equipment", label: "Camping Equipment" },
+            { value: "Audio Equipment", label: "Audio Equipment" },
+            { value: "education", label: "education" },
+            { value: "media", label: "media" },
+            { value: "Smoking device", label: "Smoking device" },
+            { value: "cellphone", label: "cellphone" },
+            { value: "kimono", label: "kimono" },
+            { value: "Brand empty box", label: "Brand empty box" },
+            { value: "Car bike equipment", label: "Car bike equipment" },
+            { value: "sewing machine", label: "sewing machine" },
+          ]}
+        />
+        <Select
+          placeholder="Chọn hãng"
+          allowClear
+          className="w-[200px]"
+          // onChange={onChooseBranch}
+          options={[
+            { value: "LOUIS VUITTON", label: "LOUIS VUITTON" },
+            { value: "CHANEL", label: "CHANEL" },
+            { value: "HERMES", label: "HERMES" },
+            { value: "GUCCI", label: "GUCCI" },
+            { value: "PRADA", label: "PRADA" },
+            { value: "ROLEX", label: "ROLEX" },
+            { value: "OMEGA", label: "OMEGA" },
+            { value: "TAG Heuer", label: "TAG Heuer" },
+            { value: "Catier", label: "Catier" },
+            { value: "BVLGARI", label: "BVLGARI" },
+            { value: "Tiffany＆Co.", label: "Tiffany＆Co." },
+            { value: "HARRY WINSTON", label: "HARRY WINSTON" },
+            { value: "Van Cleef＆Arpels", label: "Van Cleef＆Arpels" },
+            { value: "A. LANGE & SOHNE", label: "A. LANGE & SOHNE" },
+            { value: "AUDEMARS PIGUET", label: "AUDEMARS PIGUET" },
+            { value: "Baccarat", label: "Baccarat" },
+            { value: "BALENCIAGA", label: "BALENCIAGA" },
+            { value: "BALLY", label: "BALLY" },
+            { value: "BAUME＆MERCIER", label: "BAUME＆MERCIER" },
+            { value: "Bell & Ross", label: "Bell & Ross" },
+            { value: "Berluti", label: "Berluti" },
+            { value: "BOTTEGA VENETA", label: "BOTTEGA VENETA" },
+            { value: "BOUCHERON", label: "BOUCHERON" },
+            { value: "BREGUET", label: "BREGUET" },
+            { value: "BREITLING", label: "BREITLING" },
+            { value: "Buccellati", label: "Buccellati" },
+            { value: "BURBERRY", label: "BURBERRY" },
+            { value: "Carlo Parlati", label: "Carlo Parlati" },
+            { value: "Carrera y Carrera", label: "Carrera y Carrera" },
+            { value: "CASIO", label: "CASIO" },
+            { value: "CAZZANIGA", label: "CAZZANIGA" },
+            { value: "CELINE", label: "CELINE" },
+            { value: "CHARRIOL", label: "CHARRIOL" },
+            { value: "CHAUMET", label: "CHAUMET" },
+            { value: "chloe", label: "chloe" },
+            { value: "Chopard", label: "Chopard" },
+            { value: "Christian Louboutin", label: "Christian Louboutin" },
+            { value: "CHROME HEARTS", label: "CHROME HEARTS" },
+            { value: "CITIZEN", label: "CITIZEN" },
+            { value: "COACH", label: "COACH" },
+            { value: "Cole Haan", label: "Cole Haan" },
+            { value: "COMME des GARCONS", label: "COMME des GARCONS" },
+            { value: "CORUM", label: "CORUM" },
+            { value: "D＆G", label: "D＆G" },
+            { value: "Damiani", label: "Damiani" },
+            { value: "DE BEERS", label: "DE BEERS" },
+            { value: "Dior", label: "Dior" },
+            { value: "DOLCE＆GABBANA", label: "DOLCE＆GABBANA" },
+            { value: "DSQUARED3", label: "DSQUARED3" },
+            { value: "dunhill", label: "dunhill" },
+            { value: "EDOX", label: "EDOX" },
+            { value: "EMILIO PUCCI", label: "EMILIO PUCCI" },
+            { value: "ETRO", label: "ETRO" },
+            { value: "FEDERICO BUCCELLATI", label: "FEDERICO BUCCELLATI" },
+            { value: "Felisi", label: "Felisi" },
+            { value: "FENDI", label: "FENDI" },
+            { value: "FRANCK MULLER", label: "FRANCK MULLER" },
+            { value: "FRED", label: "FRED" },
+            { value: "FREDERIQUE CONSTANT", label: "FREDERIQUE CONSTANT" },
+            { value: "FURLA", label: "FURLA" },
+            { value: "Gaga Milano", label: "Gaga Milano" },
+            { value: "Georg Jensen", label: "Georg Jensen" },
+            { value: "gimel", label: "gimel" },
+            { value: "GIRARD PERREGAUX", label: "GIRARD PERREGAUX" },
+            { value: "GIVENCHY", label: "GIVENCHY" },
+            { value: "GOYARD", label: "GOYARD" },
+            { value: "GRAFF", label: "GRAFF" },
+            { value: "GRAHAM", label: "GRAHAM" },
+            { value: "HUBLOT", label: "HUBLOT" },
+            { value: "IWC", label: "IWC" },
+            { value: "Jacob & CO", label: "Jacob & CO" },
+            { value: "JAEGER LECOULTRE", label: "JAEGER LECOULTRE" },
+            { value: "Jeunet", label: "Jeunet" },
+            { value: "JEWEL STUDIO", label: "JEWEL STUDIO" },
+            { value: "JILSANDER", label: "JILSANDER" },
+            { value: "JIMMY CHOO", label: "JIMMY CHOO" },
+            { value: "Justin Davis", label: "Justin Davis" },
+            { value: "Kashikey", label: "Kashikey" },
+            { value: "Kate Spade", label: "Kate Spade" },
+            { value: "LANVIN", label: "LANVIN" },
+            { value: "LOEWE", label: "LOEWE" },
+            { value: "LONG CHAMP", label: "LONG CHAMP" },
+            { value: "LONGINES", label: "LONGINES" },
+            { value: "MACKINTOSH", label: "MACKINTOSH" },
+            { value: "MARC BY MARC JACOBS", label: "MARC BY MARC JACOBS" },
+            { value: "MARC JACOBS", label: "MARC JACOBS" },
+            { value: "MAUBOUSSIN", label: "MAUBOUSSIN" },
+            { value: "MAURICE LACROIX", label: "MAURICE LACROIX" },
+            { value: "MCM", label: "MCM" },
+            { value: "Meissen", label: "Meissen" },
+            { value: "Michael Kors", label: "Michael Kors" },
+            { value: "MIKIMOTO", label: "MIKIMOTO" },
+            { value: "miu miu", label: "miu miu" },
+            { value: "MONCLER", label: "MONCLER" },
+            { value: "MONTBLANC", label: "MONTBLANC" },
+            { value: "ORIENT", label: "ORIENT" },
+            { value: "Orobianco", label: "Orobianco" },
+            { value: "PANERAI", label: "PANERAI" },
+            { value: "PATEK PHILIPPE", label: "PATEK PHILIPPE" },
+            { value: "PIAGET", label: "PIAGET" },
+            { value: "POLA", label: "POLA" },
+            { value: "POMELLATO", label: "POMELLATO" },
+            { value: "Ponte Vecchio", label: "Ponte Vecchio" },
+            { value: "RADO", label: "RADO" },
+            { value: "RayBan", label: "RayBan" },
+            { value: "REGAL", label: "REGAL" },
+            { value: "RICHARD MILLE", label: "RICHARD MILLE" },
+            { value: "RIMOWA", label: "RIMOWA" },
+            { value: "Ritmo latino", label: "Ritmo latino" },
+            { value: "ROGER DUBUIS", label: "ROGER DUBUIS" },
+            { value: "SAINT LAURENT", label: "SAINT LAURENT" },
+            { value: "Salvatore Ferragamo", label: "Salvatore Ferragamo" },
+            { value: "SEE BY CHLOE", label: "SEE BY CHLOE" },
+            { value: "SEIKO", label: "SEIKO" },
+            { value: "Sergio Rossi", label: "Sergio Rossi" },
+            { value: "SINN", label: "SINN" },
+            { value: "SOUTHERN CROSS", label: "SOUTHERN CROSS" },
+            { value: "SUPREME", label: "SUPREME" },
+            { value: "TASAKI", label: "TASAKI" },
+            { value: "TOD’S", label: "TOD’S" },
+            { value: "Tom Ford", label: "Tom Ford" },
+            { value: "Tory Burch", label: "Tory Burch" },
+            { value: "TUDOR", label: "TUDOR" },
+            { value: "TUMI", label: "TUMI" },
+            { value: "ULYSSE NARDIN", label: "ULYSSE NARDIN" },
+            { value: "UNIVERSAL GENEVE", label: "UNIVERSAL GENEVE" },
+            { value: "UNOAERRE", label: "UNOAERRE" },
+            { value: "VACHERON CONSTANTIN", label: "VACHERON CONSTANTIN" },
+            { value: "VALENTINO", label: "VALENTINO" },
+            { value: "Vendome Aoyama", label: "Vendome Aoyama" },
+            { value: "Verite", label: "Verite" },
+            { value: "VERSACE", label: "VERSACE" },
+            { value: "Waltham", label: "Waltham" },
+            { value: "Yves Saint Laurent", label: "Yves Saint Laurent" },
+            { value: "ZENITH", label: "ZENITH" },
+            { value: "MITSUO KAJI", label: "MITSUO KAJI" },
+            { value: "Historical history", label: "Historical history" },
+            { value: "NOBUKO ISHIKAWA", label: "NOBUKO ISHIKAWA" },
+            { value: "SHUNICHI TAMURA", label: "SHUNICHI TAMURA" },
+            { value: "OTHER", label: "OTHER" },
+          ]}
+        />
+        <Select
+          placeholder="Chọn chất lượng"
+          allowClear
+          className="w-[200px]"
+          // onChange={onChooseRank}
+          options={[
+            { value: "N", label: "N" },
+            { value: "S", label: "S" },
+            { value: "A", label: "A" },
+            { value: "AB", label: "AB" },
+            { value: "B", label: "B" },
+            { value: "BC", label: "BC" },
+            { value: "C", label: "C" },
+            { value: "D", label: "D" },
+            { value: "F", label: "F" },
+            { value: "10", label: "10" },
+            { value: "9", label: "9" },
+            { value: "8", label: "8" },
+            { value: "7", label: "7" },
+            { value: "6", label: "6" },
+            { value: "5", label: "5" },
+            { value: "4", label: "4" },
+            { value: "3", label: "3" },
+            { value: "2", label: "2" },
+            { value: "1", label: "1" },
+          ]}
+        />
+        <Select
+          placeholder="Trạng thái đơn hàng"
+          allowClear
+          className="w-[200px]"
+          // onChange={onChooseRank}
+          options={[
+            { value: "N", label: "N" },
+            { value: "S", label: "S" },
+            { value: "A", label: "A" },
+            { value: "AB", label: "AB" },
+            { value: "B", label: "B" },
+            { value: "BC", label: "BC" },
+            { value: "C", label: "C" },
+            { value: "D", label: "D" },
+            { value: "F", label: "F" },
+            { value: "10", label: "10" },
+            { value: "9", label: "9" },
+            { value: "8", label: "8" },
+            { value: "7", label: "7" },
+            { value: "6", label: "6" },
+            { value: "5", label: "5" },
+            { value: "4", label: "4" },
+            { value: "3", label: "3" },
+            { value: "2", label: "2" },
+            { value: "1", label: "1" },
+          ]}
+        />
+        <DatePicker
+          placeholder="Ngày đặt"
+          // onChange={onChange}
+        />
+        <DatePicker
+          placeholder="Ngày sản phẩm"
+          // onChange={onChange}
+        />
       </div>
       <div className="desktop p-[16px] mb-[40px]">
         <Table
